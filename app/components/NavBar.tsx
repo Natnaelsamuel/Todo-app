6959f7f7c5eb0bb4302649aed787463b85a2f59f
Natnaelsamuel <natisami907@gmail.com>
2025-07-21 16:12:27 +0300

    Updated the navbar text

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { ModeToggle } from './ModeToggle'
 import { varelaRound } from '@/lib/fonts'
 import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const NavBar = () => {
   const pathname = usePathname();
+  const { status, data: session } = useSession();
   
   if (["/signin", "/signup"].includes(pathname)) {
     return null;
@@ -14,7 +16,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center py-4 px-10 shadow-xl dark:shadow-white dark:shadow-md">
         <p className={`${varelaRound.className}`}>Todos</p>
         <ul>
-          <li>Home</li>
+          { status === 'authenticated' && <li>Hello, {session.user?.name}</li>}
         </ul>
         <ModeToggle />
     </div>
